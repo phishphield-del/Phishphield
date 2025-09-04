b76781205457fd1909d1926bf5615bd85166b01e
Anubhav singh <phishphield@gmail.com>
2025-09-04 10:36:33 +0530

    Update ErrorRouter.js

--- routers/ErrorRouter.js
+++ routers/ErrorRouter.js
@@ -1,8 +1,8 @@
 import express from 'express';
-const ErrorRouter=express.Router();
-import {Errorpage404,Errorpage500} from '../controller/errorController.js'
-// ===== 404 handler =====
-ErrorRouter.get('/',Errorpage404);
-ErrorRouter.get('/',Errorpage500);
+const ErrorRouter = express.Router();
+import { Errorpage404 } from '../controller/errorController.js';
+
+
+ErrorRouter.get('*',Errorpage404);
 
 export default ErrorRouter;
